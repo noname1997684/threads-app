431f4010ece8b21db55694ea6954a4e941080d05
Hung Nguyen <duchung2811nt@gmail.com>
2025-03-04 16:06:14 +0700

    add socket deploy

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -13,7 +13,7 @@ export const SocketContextProvider=({children})=>{
     const user=useRecoilValue(userAtom)
     const [onlineUsers,setOnlineUsers]= useState([])
     useEffect(()=>{
-        const socket= io("http://localhost:5000",{
+        const socket= io("/",{
             query:{
                 userId: user?._id
             }
